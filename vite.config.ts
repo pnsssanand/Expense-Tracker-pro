e8e43ee367562d77fa71f209a94ad92ebb936cb4
Anandtravels <anandtravelsguide@gmail.com>
2025-06-02 18:19:24 +0530

    S

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  optimizeDeps: {
+    exclude: ["@rollup/rollup-linux-x64-gnu"],
+  },
+  build: {
+    rollupOptions: {
+      // Ensure Rollup doesn't try to use native extensions
+      context: "globalThis",
+    },
+  },
+});
